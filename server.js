e968c68c398acf84e6ba0d3748435f278b93a068
Yousef <youseflkc@yahoo.ca>
2022-09-18 23:48:55 -0400

    intital heroku commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/online-shop'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/online-shop/index.html'));});
 app.listen(process.env.PORT || 8080);
